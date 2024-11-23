cfcf2363991034981ec7531b85383520f2bedca7
juandmaes1 <juandmaes@unisabana.edu.co>
2024-11-23 15:44:49 -0500

    layout y creacion clases necesarias

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,3 @@
-import { View, Text } from 'react-native';
-import React from 'react';
 import { Tabs } from 'expo-router';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import { DataProvider } from '@/dataContext/DataContext';
@@ -10,43 +8,52 @@ export default function _layout() {
             <Tabs
                 screenOptions={{
                     tabBarActiveTintColor: "blue",
-                    headerShown: false
+                    headerShown: false,
                 }}
             >
                 <Tabs.Screen
-                    name='home'
+                    name="home"
                     options={{
                         title: "Inicio",
-                        tabBarIcon: ({ color, size }) => (<FontAwesome5 name="home" size={size} color={color} />)
+                        tabBarIcon: ({ color, size }) => (
+                            <FontAwesome5 name="home" size={size} color={color} />
+                        ),
                     }}
                 />
-                
                 <Tabs.Screen
-                    name='newPost'
+                    name="newPost"
                     options={{
                         title: "Nuevo Post",
-                        tabBarIcon: ({ color, size }) => (<FontAwesome5 name="edit" size={size} color={color} />)
+                        tabBarIcon: ({ color, size }) => (
+                            <FontAwesome5 name="edit" size={size} color={color} />
+                        ),
                     }}
                 />
                 <Tabs.Screen
-                    name='profile'
+                    name="profile"
                     options={{
                         title: "Perfil",
-                        tabBarIcon: ({ color, size }) => (<FontAwesome5 name="user-circle" size={size} color={color} />)
-                    }}
-                />
-                 <Tabs.Screen
-                    name='favoritos'
-                    options={{
-                        title: "Favoritos",
-                        tabBarIcon: ({ color, size }) => (<FontAwesome5 name="heart" size={size} color={color} />)
+                        tabBarIcon: ({ color, size }) => (
+                            <FontAwesome5 name="user-circle" size={size} color={color} />
+                        ),
                     }}
                 />
                 <Tabs.Screen
-                    name='ofertas'
+                    name="favoritos"
+                    options={{
+                        title: "Favoritos",
+                        tabBarIcon: ({ color, size }) => (
+                            <FontAwesome5 name="heart" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="ofertas"
                     options={{
                         title: "Ofertas",
-                        tabBarIcon: ({ color, size }) => (<FontAwesome5 name="percent" size={size} color={color} />)
+                        tabBarIcon: ({ color, size }) => (
+                            <FontAwesome5 name="percent" size={size} color={color} />
+                        ),
                     }}
                 />
             </Tabs>
